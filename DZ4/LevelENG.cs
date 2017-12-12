fba8dfb22fd62adeff9292b013c0a81c44c8cffb
Artur <33164448+Elhantero@users.noreply.github.com>
2017-12-12 12:39:59 +0200

    Add files via upload

---
+++ DZ4/LevelENG.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_4
+{
+    class LevelENG<TName, TSurname,TRank> : IEnumerable<object>, IEnumerator<object>
+    {
+        private readonly TName[] name;
+        private readonly TSurname[] surname;
+        private readonly TRank[] rank;
+        private readonly int lenght;
+
+        int position = -1;
+
+        public int Lenght
+        {
+            get { return lenght; }
+        }
+
+
+        public LevelENG(int n)
+        {
+            name = new TName[n];
+            surname = new TSurname[n];
+            rank = new TRank[n];
+            lenght = n;
+        }
+
+        //public string this[int index]
+        //{
+        //    get
+        //    {
+        //        if (index >= 0 && index < name.Length)
+        //            return rank[position]+ " - " + name[index] + " - " + surname[index];
+        //        return "Error!";
+        //    }
+        //}
+
+        public void Add(int i, TName n, TSurname s, TRank r)
+        {
+            name[i] = n;
+            surname[i] = s;
+            rank[i] = r;
+        }
+
+        public bool MoveNext()
+        {
+            if (position < name.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public object Current
+        {
+            get { return rank[position] + " - " + surname[position] + " " + name[position]; }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return rank[position] + " - " + surname[position] + " " + name[position]; }
+        }
+
+        public IEnumerator<object> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            Reset();
+        }
+    }
+}
---
+++ DZ4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            var testResult = new LevelENG<string, string, string>(10);
+
+            testResult.Add(0, "Максим", "Біловус", "A2");
+            testResult.Add(1, "Юрій", "Горчук", "A2");
+            testResult.Add(2, "Михайло", "Єфремов", "A2");
+            testResult.Add(3, "Тетяна", "Ковблюк", "C1");
+            testResult.Add(4, "Геннадій", "Кудра", "B1");
+            testResult.Add(5, "Володимир", "Лаврентьєв", "A2");
+            testResult.Add(6, "Олексій", "Матвійчук", "B1");
+            testResult.Add(7, "Олександр", "Хоменко", "A2");
+            testResult.Add(8, "Артур", "Фесенко", "B1");
+            testResult.Add(9, "Олександр", "Цимбал", "B1");
+
+            foreach (var item in testResult)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
